e7015d854b6501e3f9c80f21d015b863b4bd6f09
Nguyễn Tài <dinhtai040197@gmail.com>
2023-02-13 00:23:06 +0200

    remove dist folder

--- dist/config/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    port: 9090,
-    env: process.env.NODE_ENV || 'development',
-    /** Database settings */
-    databaseURL: process.env.DATABASE_URL
-};
-exports.default = config;
--- dist/utils/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.drawToAddScore = exports.winToAddScore = void 0;
-exports.winToAddScore = 3;
-exports.drawToAddScore = 1;
